49037d7097ee1f53e405573f3530cfb3b8c5d718
Adam Fish <af@realm.io>
2016-12-06 02:00:05 -0800

    Fix for swift carthage support

--- RealmUtilities/RealmUtilities.h
+++ RealmUtilities/RealmUtilities.h
@@ -5,3 +5,15 @@
 //  Created by Adam Fish on 12/6/16.
 //  Copyright © 2016 Adam Fish. All rights reserved.
 //
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SafeRealmObject.
+FOUNDATION_EXPORT double RealmUtilitiesVersionNumber;
+
+//! Project version string for SafeRealmObject.
+FOUNDATION_EXPORT const unsigned char RealmUtilitiesVersionString[];
+
+#import <RealmUtilities/RLMArray+Utilities.h>
+#import <RealmUtilities/RLMObject+Utilities.h>
+#import <RealmUtilities/RLMObjectBase+Utilities.h>
